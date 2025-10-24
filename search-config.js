aff309a73bcca25ad8eae86daa18d772d44c9553
HAB3X <marcus.chandler@griffithuni.edu.au>
2025-10-25 09:34:04 +1000

    Add search configuration options

---
+++ search-config.js
@@ -0,0 +1,18 @@
+// search-config.js
+const SEARCH_OPTIONS = {
+  categories: [
+    "Fashion & Apparel",
+    "Health & Wellness",
+    "Food & Beverage",
+    "Home & Furniture",
+    "Energy",
+    "Agriculture",
+    "Transport",
+    "Finance",
+    "Government"
+  ],
+  placeholder: "Search companies, sectors, or SDGs...",
+  minChars: 2
+};
+
+export default SEARCH_OPTIONS;
